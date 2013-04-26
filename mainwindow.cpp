649b5aa99cf24c98ec3955008faf9bc86648a24b
Trevor Reed <trevorar@usc.edu>
2013-04-26 02:05:42 -0700

    Worked on mainwindow class and connected timer. Also, sped up the game over time. close #13 close #18

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,31 +2,55 @@
 
 /** Default constructor for MainWindow */
 MainWindow::MainWindow() {
-		error = new QErrorMessage;
-		
-		//MENU BAR
-		/*mb = menuBar();
-		file = new QMenu("File");
-		
-    quit = new QAction("Quit", file);
-    start = new QAction("Start Game", file);
-    
-    file->addAction(start);
-    file->addAction(quit);
-       
-    connect(quit, SIGNAL(triggered()), qApp, SLOT(quit()));
-    connect(start, SIGNAL(triggered()), this, SLOT(pressStart()));*/
-    
-    //mb->addMenu(file);
-    
-    //TOOL BAR
-    //toolBar = new QToolBar();
-    
-    //GAME BOARD
-    game = new GraphicsWindow();
-    setCentralWidget(game->getView());
-		
-		this->setMinimumSize(1200, 700);
+	error = new QErrorMessage;
+	
+	//MENU BAR
+	mb = menuBar();
+	file = new QMenu("File");
+	
+  quit = new QAction("Quit", file);
+  start = new QAction("Start Game", file);
+  
+  file->addAction(start);
+  file->addAction(quit);
+     
+  connect(quit, SIGNAL(triggered()), qApp, SLOT(quit()));
+  connect(start, SIGNAL(triggered()), this, SLOT(startSlot()));
+  
+  mb->addMenu(file);
+  
+  //TOOL BAR
+  toolBar = new QToolBar();
+  
+  startButton = new QPushButton("Start (F1)");
+  quitButton = new QPushButton("Quit (Esc)");
+  pauseButton = new QPushButton("Pause (P)");
+  optionButton = new QPushButton("Options");
+  
+  connect(startButton, SIGNAL(clicked()), this, SLOT(startSlot()));
+  connect(quitButton, SIGNAL(clicked()), qApp, SLOT(quit()));
+  connect(pauseButton, SIGNAL(clicked()), this, SLOT(pause()));
+  connect(optionButton, SIGNAL(clicked()), this, SLOT(options()));
+  
+  toolBar->addWidget(startButton);
+  toolBar->addSeparator();
+  toolBar->addWidget(quitButton);
+  toolBar->addSeparator();
+  toolBar->addWidget(pauseButton);
+  toolBar->addSeparator();
+  toolBar->addWidget(optionButton);
+  
+  addToolBar(toolBar);
+  
+  //GAME BOARD
+  game = new GraphicsWindow();
+  setCentralWidget(game);
+	
+	setMinimumSize(1200, 700);
+	setMaximumSize(1200, 700);
+	
+	paused = 0;
+	setFocus();
 }
 
 /** Deconstructor - deallocates all memory from the MainWindow */
@@ -34,10 +58,9 @@ MainWindow::~MainWindow() {
 	delete error;
 
 	//Deallocation of menu bar
-	/*delete start;
+	delete start;
 	delete quit;
-	delete file;*/
-
+	delete file;
 	
 	//Deallocation of game board
 	delete game;
@@ -46,4 +69,50 @@ MainWindow::~MainWindow() {
 /** Displays the MainWindow */
 void MainWindow::show() {
 	QMainWindow::show();
+}
+
+void MainWindow::startSlot() { 
+	if(game->getTimer()->isActive() || paused) {
+		delete game;
+		game = new GraphicsWindow;
+		setCentralWidget(game);
+	}
+	game->start();	
+	setFocus();
+}
+
+void MainWindow::pause() {
+	if(game->getTimer()->isActive()) {
+		game->getTimer()->stop();
+	} else {
+		game->getTimer()->start();
+	}
+	paused = !paused;
+	setFocus();
+}
+
+void MainWindow::options() { }
+
+void MainWindow::keyPressEvent(QKeyEvent *e) {
+	if(e->key() ==  Qt::Key_Right) {
+		game->getNinja()->moveRight();
+	}
+	if(e->key() == Qt::Key_Left) {
+		game->getNinja()->moveLeft();
+	}
+	if(e->key() == Qt::Key_P) {
+		pause();
+	}
+	if(e->key() == Qt::Key_F1) {
+		startSlot();
+	}
+	if(e->key() == Qt::Key_Escape) {
+		qApp->quit();
+	}
+}
+
+void MainWindow::keyReleaseEvent(QKeyEvent *e) {
+	if(e->key() ==  Qt::Key_Right || e->key() == Qt::Key_Left) {
+		game->getNinja()->stop();
+	}
 }
--- mainwindow.h
+++ mainwindow.h
@@ -13,23 +13,20 @@
 #include <QTimeLine>
 #include <QGraphicsItemAnimation>
 #include <QPushButton>
-#include <QLineEdit>
-#include <QPlainTextEdit>
 #include <QMenu>
 #include <QMenuBar>
 #include <QAction>
 #include <QApplication>
 #include <QToolBar>
-#include <QRadioButton>
 #include <QDockWidget>
 #include <QErrorMessage>
-#include <QListWidget>
-#include <QComboBox>
 #include <QLabel>
+#include <QKeyEvent>
 #include <cmath>
 #include <cstdlib>
 
 #include "graphicswindow.h"
+#include "ninja.h"
 
 class MainWindow : public QMainWindow {
 	Q_OBJECT
@@ -42,25 +39,33 @@ class MainWindow : public QMainWindow {
   
 	private:	
 		//Menu Bar components
-		/*QMenuBar *mb;
+		QMenuBar *mb;
 		QMenu *file;
 		QAction *start;
-		QAction *quit;*/
+		QAction *quit;
 		
 		//Tool bar components
-		//QToolBar *toolBar;
+		QToolBar *toolBar;
+		QPushButton *startButton;
+		QPushButton *quitButton;
+		QPushButton *pauseButton;
+		bool paused;
+		QPushButton *optionButton;
 		
 		//Central widget components
 		GraphicsWindow *game;
 		
 		//Message for errors, etc.
 		QErrorMessage *error;
-		
-		//Bottom dock widget components - Text box to display errors
-		/*QDockWidget *outputs;
-		QPlainTextEdit *textBox;*/
+    
+    //Keyboard commands
+    void keyPressEvent(QKeyEvent *e);
+    void keyReleaseEvent(QKeyEvent *e);
 
 	public slots:
+		void startSlot();
+		void pause();
+		void options();
 };
 
 #endif
